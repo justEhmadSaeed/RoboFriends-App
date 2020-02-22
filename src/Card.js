389a9b505117305ad40ba07347e1a7ec94b5463b
Ehmad <justEhmadSaeed@gmail.com>
2020-02-23 03:21:07 +0500

    ES6 Conversion & looping in React

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
-        <div className = 'bg-light-blue tc dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${props.id}?150x150`} alt = 'robots'></img>
+        <div className='bg-light-blue tc dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}?150x150`} alt='robots'></img>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import 'tachyons';
 import { robots } from './robots';
 ReactDOM.render(
     <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        {robots.map((robo, i) => <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />)}
     </div>
     , document.getElementById('root'));
 
