8d8b3911e6f1cae81d646f518115604c75532c9d
Ehmad <justEhmadSaeed@gmail.com>
2020-02-23 02:18:54 +0500

    Initial Commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src='' alt = 'robots'></img>
+            <div>
+                <h2>Ehmad Saeed</h2>
+                <p>justehmadsaeed@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
