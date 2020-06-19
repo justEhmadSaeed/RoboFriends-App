86f19cd7f9ece8aad74ca3d2ae34d178b5d8e1b6
Ehmad <justEhmadSaeed@gmail.com>
2020-06-20 00:46:52 +0500

    👌 IMPROVE: Add CardList instead of Cards

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Card from './Card';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-import { robots } from './robots';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from "./robots";
+import CardList from "./CardList";
 
-ReactDOM.render(
-    <div>
-        {robots.map((robo, i) => <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />)}
-    </div>
-    , document.getElementById('root'));
+ReactDOM.render(<CardList robots = {robots} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
