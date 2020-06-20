c8acb1cbd7e7e85b4d5f766aa845ac208ef7d9ad
Ehmad <justEhmadSaeed@gmail.com>
2020-06-20 18:09:33 +0500

    📦 NEW: Add Parent App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+
+const App = () => {
+  return (
+    <div>
+      <h1>RoboFriends</h1>
+      <CardList robots={robots} />
+    </div>
+  );
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
-import { robots } from "./robots";
-import CardList from "./CardList";
+import App from "./App";
 
-ReactDOM.render(<CardList robots = {robots} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
