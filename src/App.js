50c13c38fab91c0e0177e07219631188621060a6
Ehmad <justEhmadSaeed@gmail.com>
2020-06-21 00:57:26 +0500

    📦 NEW: Add SearchBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
-    <div>
+    <div className = 'tc'>
       <h1>RoboFriends</h1>
+      <SearchBox />
       <CardList robots={robots} />
     </div>
   );
