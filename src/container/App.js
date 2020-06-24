1b0a30478431977e74a05d08cff40b623c47d656
Ehmad <justEhmadSaeed@gmail.com>
2020-06-24 13:13:07 +0500

    👌 IMPROVE: Modularizing Files

--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
-import App from "./App";
+import App from "./container/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
