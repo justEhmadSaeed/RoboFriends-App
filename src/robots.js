9e45c92e450e90980142f0a9e86445231a5d2b34
Ehmad <justEhmadSaeed@gmail.com>
2020-06-26 23:47:01 +0500

    👌 IMPROVE: Code Indentation

--- src/robots.js
+++ src/robots.js
@@ -1,62 +1,62 @@
 export const robots = [
-    {
-        id: 1,
-        name: 'Steel',
-        username: 'Steel',
-        email: 'maxsteel@gmail.com'
-    },
-    {
-        id: 2,
-        name: 'Opntron',
-        username: 'opntron',
-        email: 'opntron@gmail.com'
-    },
-    {
-        id: 3,
-        name: 'Bracer',
-        username: 'bracer',
-        email: 'bracer@gmail.com'
-    },
-    {
-        id: 4,
-        name: 'Gearz',
-        username: 'gearz',
-        email: 'gearz@gmail.com'
-    },
-    {
-        id: 5,
-        name: 'Awox',
-        username: 'awox',
-        email: 'awox@gmail.com'
-    },
-    {
-        id: 6,
-        name: 'Rob Bott',
-        username: 'Robbott',
-        email: 'robbott@gmail.com'
-    },
-    {
-        id: 7,
-        name: 'Andromeda',
-        username: 'Andromeda',
-        email: 'andromeda@gmail.com'
-    },
-    {
-        id: 8,
-        name: 'Egomoid',
-        username: 'Egomoid',
-        email: 'egomoid@gmail.com'
-    },
-    {
-        id: 9,
-        name: 'Fiber',
-        username: 'fiber',
-        email: 'fiber@gmail.com'
-    },
-    {
-        id: 10,
-        name: 'Bult',
-        username: 'bult',
-        email: 'bult@gmail.com'
-    },
-] 
+  {
+    id: 1,
+    name: "Steel",
+    username: "Steel",
+    email: "maxsteel@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Opntron",
+    username: "opntron",
+    email: "opntron@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Bracer",
+    username: "bracer",
+    email: "bracer@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Gearz",
+    username: "gearz",
+    email: "gearz@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Awox",
+    username: "awox",
+    email: "awox@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Rob Bott",
+    username: "Robbott",
+    email: "robbott@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Andromeda",
+    username: "Andromeda",
+    email: "andromeda@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Egomoid",
+    username: "Egomoid",
+    email: "egomoid@gmail.com",
+  },
+  {
+    id: 9,
+    name: "Fiber",
+    username: "fiber",
+    email: "fiber@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Bult",
+    username: "bult",
+    email: "bult@gmail.com",
+  },
+];
