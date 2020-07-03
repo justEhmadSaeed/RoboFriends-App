cb2ab1bb66ec921148b70be490ac38f0cc7b6bb1
Ehmad <justEhmadSaeed@gmail.com>
2020-07-03 22:38:39 +0500

    👌 IMPROVE: Pass store to the App

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { searchRobots } from './reducers';
 
 const store = createStore(searchRobots);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App store = {store} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
