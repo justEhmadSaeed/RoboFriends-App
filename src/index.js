df2186ee4726ccf675a5e3811b458746e670e886
Ehmad <justEhmadSaeed@gmail.com>
2020-07-06 22:59:10 +0500

    👌 IMPROVE: Use provider for state management

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 import App from "./container/App";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
 
 const store = createStore(searchRobots);
 
-ReactDOM.render(<App store = {store} />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
