1eca9b0d49046b70af602aa88d3732e75b7efc9f
Ehmad Saeed <justehmadsaeed@gmail.com>
2021-03-24 16:44:37 +0500

    👌 IMPROVE: Update Styling

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Scroll = (props) => {
-  return (
-    <div style={{ overflowY: "scroll", border: "1px solid black", height: '500px' }}>
-      {props.children}
-    </div>
-  );
-};
+	return (
+		<div style={{ overflowY: 'scroll', maxHeight: '400px' }}>
+			{props.children}
+		</div>
+	)
+}
 
-export default Scroll;
+export default Scroll
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
-        className="pa3 ba b--green bg-lightest-blue"
+        className="search-box br3 pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
         onChange={searchChange}
